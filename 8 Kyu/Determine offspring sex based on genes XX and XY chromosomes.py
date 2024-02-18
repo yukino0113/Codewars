4af8ee1ed324ca7c6e40b2d6ac8aff7d30bb8fd7
Yukino0113 <jethro0113@gmail.com>
2024-02-18 19:03:12 +0800

    Determine offspring sex based on genes XX and XY chromosomes

---
+++ 8 Kyu/Determine offspring sex based on genes XX and XY chromosomes.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/56530b444e831334c0000020/train/python
+
+def chromosome_check(chromosome):
+    return 'Congratulations! You\'re going to have a son.' if 'Y' in chromosome else 'Congratulations! You\'re going to have a daughter.'
+
